0f14ba6f83974d4857584db706072d9a3fa49649
nawbc <wanghan9423@outlook.com>
2024-09-27 10:43:54 +0800

    macos logo

---
+++ packages/video-editor/src/preset/PublishVideoEditor.tsx
@@ -0,0 +1,7 @@
+import { Box } from '@mantine/core';
+
+export const PublishVideoEditor = function () {
+  return (
+    <Box m="0 auto" px="xl" maw="var(--publish-doc-editor-max-width)"></Box>
+  );
+};
